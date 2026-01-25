abb73531780ddd489f8048ed21ff2981b00982d5
avi rapa <kenblock.ar@gmail.com>
2026-01-25 14:36:59 +0200

    עדכון קובץ ראשי לכל הפעולות

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #מחשבון פשוט
 #גרסה 1.0
 
-from operations import add, subtract
+from operations import add, subtract, multiply, divide
 
 
 print("!ברוכים הבאים למחשבון")
 print("====================")
 
 
-# דוגמאות
-result1 = add(5, 3)
-print(f"5 + 3 = {result1}")
-result2 = subtract(10, 4)
-print(f"10 - 4 = {result2}")
+# דוגמאות לכל הפעולות
+print(f"5 + 3 = {add(5, 3)}")
+print(f"10 - 4 = {subtract(10, 4)}")
+print(f"6 * 7 = {multiply(6, 7)}")
+print(f"20 / 4 = {divide(20, 4)}")
+print(f"10 / 0 = {divide(10, 0)}")
