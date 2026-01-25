d385d63cb523381618df11c1d33a5f9b3b4f0efd
avi rapa <kenblock.ar@gmail.com>
2026-01-25 14:28:29 +0200

    connect main to operation

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #מחשבון פשוט
 #גרסה 1.0
 
+from operations import add, subtract
+
+
 print("!ברוכים הבאים למחשבון")
 print("====================")
+
+
+# דוגמאות
+result1 = add(5, 3)
+print(f"5 + 3 = {result1}")
+result2 = subtract(10, 4)
+print(f"10 - 4 = {result2}")
